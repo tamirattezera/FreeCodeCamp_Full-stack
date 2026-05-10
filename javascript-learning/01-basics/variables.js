c3aa2a9073217abb21cfe08ff5bbe2bfe5829e16
Tamirat Tezera <tamirattezera8@gmail.com>
2026-05-10 07:26:37 +0300

    Practice JavaScript strings, variables, and console output

---
+++ javascript-learning/01-basics/variables.js
@@ -0,0 +1,45 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "CodeBot";
+const botLocation = "the Cloud";
+const favoriteLanguage = "JavaScript";
+
+console.log(`My name is ${botName} and I live on ${botLocation}.`);
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+
+let codingFact = `${favoriteLanguage} was created in just 10 days!`;
+console.log(codingFact);
+
+codingFact = `${favoriteLanguage} is one of the core technologies of the web, along with HTML and CSS.`;
+console.log(codingFact);
+
+codingFact = `${favoriteLanguage} can run both in the browser and on servers using Node.js.`;
+console.log(codingFact);
+
+console.log(
+  `It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`,
+);
+
+//########################################################
+
+let adjective = "brave";
+let noun = "dragon";
+let verb = "angry";
+let place = "mountain";
+let adjective2 = "fiery";
+let noun2 = "marshmallows";
+
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log(`First story: ${firstStory}`);
+
+adjective = "curious";
+noun = "robot";
+verb = "excited";
+place = "futuristic city";
+adjective2 = "glowing";
+noun2 = "electric snacks";
+
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log(`Second story: ${secondStory}`);
